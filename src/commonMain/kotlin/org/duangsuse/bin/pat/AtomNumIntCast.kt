261bf17cbf0057490daf1e91631cf1e53fc937f6
duangsuse <fedora-opensuse@outlook.com>
2020-01-15 11:02:39 +0800

    Fix(AtomNumIntCast): widen/narrow overload conflicting

--- src/commonMain/kotlin/org/duangsuse/bin/pat/AtomNumIntCast.kt
+++ src/commonMain/kotlin/org/duangsuse/bin/pat/AtomNumIntCast.kt
@@ -4,18 +4,18 @@ import org.duangsuse.bin.*
 
 abstract class Int32Pattern: Pattern.Sized<Int32>
 
-fun Pattern.Sized<Int8>.widen() = object: Int32Pattern() {
-  override fun read(s: Reader): Int32 = this@widen.read(s).toInt()
-  override fun write(s: Writer, x: Int32) = this@widen.write(s, x.toByte())
-  override val size: Cnt? get() = this@widen.size
+fun Pattern.Sized<Int8>.widen8() = object: Int32Pattern() {
+  override fun read(s: Reader): Int32 = this@widen8.read(s).toInt()
+  override fun write(s: Writer, x: Int32) = this@widen8.write(s, x.toByte())
+  override val size: Cnt? get() = this@widen8.size
 }
-fun Pattern.Sized<Int16>.widen() = object: Int32Pattern() {
-  override fun read(s: Reader): Int32 = this@widen.read(s).toInt()
-  override fun write(s: Writer, x: Int32) = this@widen.write(s, x.toShort())
-  override val size: Cnt? get() = this@widen.size
+fun Pattern.Sized<Int16>.widen16() = object: Int32Pattern() {
+  override fun read(s: Reader): Int32 = this@widen16.read(s).toInt()
+  override fun write(s: Writer, x: Int32) = this@widen16.write(s, x.toShort())
+  override val size: Cnt? get() = this@widen16.size
 }
-fun Pattern.Sized<Int64>.narrow() = object: Int32Pattern() {
-  override fun read(s: Reader): Int32 = this@narrow.read(s).toInt()
-  override fun write(s: Writer, x: Int32) = this@narrow.write(s, x.toLong())
-  override val size: Cnt? get() = this@narrow.size
+fun Pattern.Sized<Int64>.narrow64() = object: Int32Pattern() {
+  override fun read(s: Reader): Int32 = this@narrow64.read(s).toInt()
+  override fun write(s: Writer, x: Int32) = this@narrow64.write(s, x.toLong())
+  override val size: Cnt? get() = this@narrow64.size
 }
