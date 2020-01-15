abc38936435922ad45b7dafa95494c8175ea93c8
duangsuse <fedora-opensuse@outlook.com>
2020-01-16 00:21:11 +0800

    Test(JavaClass): Constant pool

--- src/commonTest/kotlin/org/duangsuse/bin/pat/AbstractJavaClassTests.kt
+++ src/commonTest/kotlin/org/duangsuse/bin/pat/AbstractJavaClassTests.kt
@@ -6,6 +6,6 @@ import kotlin.test.Test
 abstract class AbstractJavaClassTests(private val s: Reader) {
   @Test fun read() {
     val res = ClassFile.read(s)
-    println(res)
+    println(res.constants.joinToString("\n"))
   }
 }
--- src/commonTest/kotlin/org/duangsuse/bin/pat/JavaClass.kt
+++ src/commonTest/kotlin/org/duangsuse/bin/pat/JavaClass.kt
@@ -18,15 +18,99 @@ private val u4 = nat32
 typealias U8 = Int64
 private val u8 = int64
 
-class ClassFile: AnyTuple(2) {
+class ClassFile: AnyTuple(3) {
   companion object: BigEndian<ClassFile>(Seq(::ClassFile,
-    +(nat32 magic 0xcafebabeL),
-    +Version
+    +(u4 magic 0xcafebabeL),
+    +Version,
+    +ConstantInfo.array(PAIR_NIL, u2 padding (-1))
   ))
   class Version: IntTuple(2) {
     companion object: Seq<Version, Nat16>(::Version,
       u2, u2)
     val minorVersion by index(0)
     val majorVersion by index(1)
+  }
+  var magic by indexOf<U4>(0)
+  var version by indexOf<Version>(1)
+  var constants by indexOf<Array<Pair<Idx, ConstantInfo>>>(2)
+}
+val PAIR_NIL: Pair<Idx, Any> = Pair(-1, 0)
+
+sealed class ConstantInfo(size: Cnt): AnyTuple(size) {
+  companion object: AnyCond(u1,
+    +Unknown, +KstUTF8, +Unknown, +ConstantValue.KstInt, +ConstantValue.KstFloat, +ConstantValue.KstLong, +ConstantValue.KstDouble,
+    +KstClassRef, +KstString, +KstFieldRef, +KstMethodRef,
+    +KstInterfaceMethodRef, +KstNameAndType, +Unknown, +Unknown,
+    +KstMethodHandle, +KstMethodType, +Unknown, +KstInvokeDynamic)
+  class KstUTF8: ConstantInfo(1) {
+    companion object: AnySeq<KstUTF8>(::KstUTF8, +u2.sizedByteArray())
+    var rawString by indexOf<ByteArray>(0)
+    @ExperimentalStdlibApi
+    var value get() = rawString.decodeToString()
+      set(s) { rawString = s.encodeToByteArray() }
+    @ExperimentalStdlibApi
+    override fun toString(): String = value
+  }
+  sealed class ConstantValue<T>: ConstantInfo(1) {
+    abstract var value: T
+    override fun toString(): String = "Constant($value)"
+    class KstInt : ConstantValue<Int32>() {
+      companion object : AnySeq<KstInt>(::KstInt, +int32)
+      override var value by indexOf<Int32>(0)
+    }
+    class KstFloat : ConstantValue<Rat32>() {
+      companion object : AnySeq<KstFloat>(::KstFloat, +rat32)
+      override var value by indexOf<Rat32>(0)
+    }
+    class KstLong : ConstantValue<Int64>() {
+      companion object : AnySeq<KstLong>(::KstLong, +int64)
+      override var value by indexOf<Int64>(0)
+    }
+    class KstDouble : ConstantValue<Rat64>() {
+      companion object : AnySeq<KstDouble>(::KstDouble, +rat64)
+      override var value by indexOf<Rat64>(0)
+    }
+  }
+  class KstClassRef: ConstantInfo(1) {
+    companion object: AnySeq<KstClassRef>(::KstClassRef, +u2)
+    var index by indexOf<U2>(0)
+  }
+  class KstString: ConstantInfo(1) {
+    companion object: AnySeq<KstString>(::KstString, +u2)
+    var index by indexOf<U2>(0)
+  }
+  class KstFieldRef: ConstantInfo(2) {
+    companion object: AnySeq<KstFieldRef>(::KstFieldRef, +u2, +u2)
+    var refClass by indexOf<U2>(0)
+    var refNameType by indexOf<U2>(1)
+  }
+  class KstMethodRef: ConstantInfo(2) {
+    companion object: AnySeq<KstMethodRef>(::KstMethodRef, +u2, +u2)
+    var refClass by indexOf<U2>(0)
+    var refNameType by indexOf<U2>(1)
+  }
+  class KstInterfaceMethodRef: ConstantInfo(2) {
+    companion object: AnySeq<KstInterfaceMethodRef>(::KstInterfaceMethodRef, +u2, +u2)
+    var refClass by indexOf<U2>(0)
+    var refNameType by indexOf<U2>(1)
+  }
+  class KstNameAndType: ConstantInfo(2) {
+    companion object: AnySeq<KstNameAndType>(::KstNameAndType, +u2, +u2)
+    var refName by indexOf<U2>(0)
+    var refDescriptor by indexOf<U2>(1)
+  }
+  class KstMethodHandle: ConstantInfo(2) {
+    companion object: AnySeq<KstMethodHandle>(::KstMethodHandle, +u1, +u2)
+    var refKind by indexOf<U1>(0)
+    var refIndex by indexOf<U2>(1)
+  }
+  class KstMethodType: ConstantInfo(1) {
+    companion object: AnySeq<KstMethodType>(::KstMethodType, +u2)
+    var refDescriptor by indexOf<U2>(0)
+  }
+  class KstInvokeDynamic: ConstantInfo(2) {
+    companion object: AnySeq<KstInvokeDynamic>(::KstInvokeDynamic, +u2, +u2)
+    var refBootstrapMethodAttr by indexOf<U2>(0)
+    var refNameAndType by indexOf<U2>(1)
   }
 }
