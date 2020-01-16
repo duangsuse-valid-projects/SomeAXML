8e30bc892d62fa3321a4f353e7b7f1885cd0bd4f
duangsuse <fedora-opensuse@outlook.com>
2020-01-16 11:18:07 +0800

    Improve(pat.extra.Extensions): extensions for bin.pat.basic

--- src/commonMain/kotlin/org/duangsuse/bin/pat/extra/Extensions.kt
+++ src/commonMain/kotlin/org/duangsuse/bin/pat/extra/Extensions.kt
@@ -1,11 +1,19 @@
 package org.duangsuse.bin.pat.extra
 
 import org.duangsuse.bin.Cnt
+import org.duangsuse.bin.Idx
 import org.duangsuse.bin.Reader
 import org.duangsuse.bin.Writer
 import org.duangsuse.bin.pat.BitFlags
 import org.duangsuse.bin.pat.BitFlags32Creator
 import org.duangsuse.bin.pat.Pattern
+import org.duangsuse.bin.pat.basic.Cond
+import org.duangsuse.bin.pat.basic.Repeat
+
+//// basic patterns
+
+infix fun <T: Any> Pattern<Cnt>.sizedRepeat(item: Pattern<T>) = Repeat(this, item)
+fun <T> Pattern<Idx>.cond(vararg conditions: Pattern<T>) = Cond(this, *conditions)
 
 //// HelperPattern
 
