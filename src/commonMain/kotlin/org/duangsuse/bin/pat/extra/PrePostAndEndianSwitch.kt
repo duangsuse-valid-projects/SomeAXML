c820e74cbbc4571e2355eba724e5357ff72b35c7
duangsuse <fedora-opensuse@outlook.com>
2020-01-16 17:12:21 +0800

    Arch(pat): PrePostAndEndianSwitch rename file & change open/final, typo

--- src/commonMain/kotlin/org/duangsuse/bin/pat/extra/PrePostAndEndianSwitch.kt
+++ src/commonMain/kotlin/org/duangsuse/bin/pat/extra/PrePostAndEndianSwitch.kt
@@ -5,13 +5,13 @@ import org.duangsuse.bin.Reader
 import org.duangsuse.bin.Writer
 import org.duangsuse.bin.pat.Pattern
 
-/** Add argument/return listen for [Pattern] read/write */
+/** Add on call/return listener for [Pattern] read/write */
 abstract class PrePost<T>(private val item: Pattern<T>): Pattern.BySizedFully<T>(item) {
-  override fun read(s: Reader): T {
+  final override fun read(s: Reader): T {
     onReadPre(s)
     return item.read(s).also { onReadPost(s, it) }
   }
-  override fun write(s: Writer, x: T) {
+  final override fun write(s: Writer, x: T) {
     onWritePre(s, x)
     item.write(s, x).also { onWritePost(s) }
   }
@@ -25,21 +25,21 @@ abstract class PrePost<T>(private val item: Pattern<T>): Pattern.BySizedFully<T>
 /** Read/write child [item] in byte order of [newEndian], then restore old order.
  *
  * NOTE: Thread safety __IS NOT__ guaranteed */
-open class EndianSwitch<T>(item: Pattern<T>, private val newEndian: ByteOrder): PrePost<T>(item) {
-  private lateinit var oldByteOrder: ByteOrder
+sealed class EndianSwitch<T>(item: Pattern<T>, private val newEndian: ByteOrder): PrePost<T>(item) {
+  private lateinit var old: ByteOrder
   override fun onReadPre(s: Reader) {
-    oldByteOrder = s.byteOrder
+    old = s.byteOrder
     s.byteOrder = newEndian
   }
   override fun onReadPost(s: Reader, result: T) {
-    s.byteOrder = oldByteOrder
+    s.byteOrder = old
   }
   override fun onWritePre(s: Writer, x: T) {
-    oldByteOrder = s.byteOrder
+    old = s.byteOrder
     s.byteOrder = newEndian
   }
   override fun onWritePost(s: Writer) {
-    s.byteOrder = oldByteOrder
+    s.byteOrder = old
   }
   open class BigEndian<T>(item: Pattern<T>): EndianSwitch<T>(item, ByteOrder.BigEndian)
   open class LittleEndian<T>(item: Pattern<T>): EndianSwitch<T>(item, ByteOrder.LittleEndian)
